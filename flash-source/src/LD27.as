1ce2ef547134aa196ef3a3e9d6e656e976771155
kasper <barathrumm@gmail.com>
2013-08-24 00:58:21 +0200

    initial commit

---
+++ flash-source/src/LD27.as
@@ -0,0 +1,12 @@
+package
+{
+	import flash.display.Sprite;
+	
+	public class LD27 extends Sprite
+	{
+		public function LD27()
+		{
+			
+		}
+	}
+}
---
+++ web/js/app.js
@@ -0,0 +1,11 @@
+(function() {
+
+/**
+ * Initiate the flash etc on page
+ * @return void
+ */
+	$(document).on('ready', function() {
+		swfobject.embedSWF("../flash-source/bin-debug/LD27.swf", "flash-container", "100%", "600", "11.4.0", "swf/expressInstall.swf");
+	});
+
+})();
