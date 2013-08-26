abfe2dd6955860662c634f8bddda25fcc4ffe584
kasper <barathrumm@gmail.com>
2013-08-26 02:44:57 +0200

    bumb

--- web/js/app.js
+++ web/js/app.js
@@ -5,7 +5,7 @@
  * @return void
  */
 	$(document).on('ready', function() {
-		swfobject.embedSWF("../flash-source/bin-debug/Main.swf", "flash-container", "100%", "600", "11.4.0", "swf/expressInstall.swf");
+		swfobject.embedSWF("../flash-source/bin-release/Main.swf", "flash-container", "100%", "600", "11.4.0", "swf/expressInstall.swf");
 	});
 
 })();
